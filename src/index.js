2ae412821296803376b58f65e6783b06e89a2dfa
Tiago Zini <tiagozini@gmail.com>
2026-05-09 00:04:32 -0300

    style: Improved css style reset browser style and adjusting the game style considering the game window dimension

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StrictMode } from "react";
 import ReactDOM from 'react-dom';
+import './normalize.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
